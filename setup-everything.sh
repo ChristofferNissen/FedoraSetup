be5acc562ca5c117df789823b3d870563870d37a
Christoffer Nissen <christoffer.nissen@gmail.com>
2020-09-17 02:33:10 +0200

    i3 initial implementation done

--- setup-everything.sh
+++ setup-everything.sh
@@ -30,6 +30,9 @@
 # # Desktop environment
 # ./install_i3.sh
 
+# i3-gaps dependencies
+sudo dnf install libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock
+
 # i3-gaps
 DIR="../i3-gaps/"
 if [ -d "$DIR" ]; then
