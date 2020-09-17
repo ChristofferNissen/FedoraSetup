d8882ac760d789a681c32f370f565ef7e059bfb5
Christoffer Nissen <christoffer.nissen@gmail.com>
2020-09-17 02:16:31 +0200

    added i3-gaps install

--- setup-everything.sh
+++ setup-everything.sh
@@ -28,6 +28,40 @@ fi
 
 ./install_i3.sh
 
+# i3-gaps
+DIR="../i3-gaps/"
+if [ -d "$DIR" ]; then
+  echo "i3-gaps already exists"
+  cd i3-gaps
+  git checkout gaps && git pull
+  autoreconf --force --install
+  rm -rf build
+  mkdir build
+  cd build
+  ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+  make
+  sudo make install
+  cd ..
+  cd FedoraSetup
+else 
+  # Take action if $DIR doesnt exists. #
+  # run script in dotfiles to create symlinks
+  echo "Cloning dotfiles.."
+  cd ..
+  git clone https://www.github.com/Airblader/i3 i3-gaps
+  cd i3-gaps
+  git checkout gaps && git pull
+  autoreconf --force --install
+  rm -rf build
+  mkdir build
+  cd build
+  ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+  make
+  sudo make install
+  cd ..
+  cd FedoraSetup
+fi
+
 # Work Apps
 ./install_alacritty.sh
 ./install_slack.sh
